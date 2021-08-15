f3f6ac8bf2aa9800b101177d25778f24d5a4f472
lbouweriks <88744104+lbouweriks@users.noreply.github.com>
2021-08-15 08:45:30 +0200

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,11 @@
+function paintWall(colour){
+    console.log("the wall has been painted " + colour)
+}
+
+paintWall("green")
+
+function paintXWall(colour, wall){
+    console.log("the " + wall + " has been painted " + colour)
+}
+
+paintXWall("red", "south")
