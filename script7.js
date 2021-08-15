e9a56a5d0501c0a2034ed056dcbe8c940b2c742c
lbouweriks <88744104+lbouweriks@users.noreply.github.com>
2021-08-15 09:48:51 +0200

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,17 @@
+function squareSquare(number1, number2){
+    number1=number1*number1
+    number2=number2*number2
+    return (number1+number2) * (number1+number2)
+}
+
+const squareSquare2 = function(number1, number2){
+    number1=number1*number1
+    number2=number2*number2
+    return (number1+number2) * (number1+number2)
+}
+
+const squareSquare3 = (number1, number2) => {
+    number1=number1*number1
+    number2=number2*number2
+    return (number1+number2) * (number1+number2)
+}
